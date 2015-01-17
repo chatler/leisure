33abb57ac83c336595c687d70683474243c77fba
chatler <zhangquan@vip.163.com>
2015-01-17 14:23:12 +0800

    add a negative or positive value judgement

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
 print "hello shit guy!!!!"
-name = raw_input()
-print name
+
+# test for raw input, just as cin in cpp
+#name = raw_input()
+#print name
+
+#print the absolute value of integers
+a = 100
+if a >= 0:
+	print "a > 0"
+	print a
+else:
+	print "a < 0"
+	print -a
