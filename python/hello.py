73c7264b42b9733c8983a99002f18d31e5325748
chatler <zhangquan@vip.163.com>
2015-01-17 14:56:16 +0800

    add some print statements

--- python/hello.py
+++ python/hello.py
@@ -14,3 +14,21 @@ if a >= 0:
 else:
 	print "a < 0"
 	print -a
+
+#print the float point number
+print 1.2e8
+
+#print the string
+print "i'm your darling"
+print 'i\'m your darling'
+print 'i\\\'m your darling'
+print r'i\\\'m your darling'
+
+#escape or non-escape characters
+print 'end'
+
+
+
+
+
+
