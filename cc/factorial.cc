49908dcb8515d50650ed9d9aee8659e0241ba331
chatler <zhangquan@vip.163.com>
2015-03-18 08:04:27 -0700

    add factorial function

--- cc/factorial.cc
+++ cc/factorial.cc
@@ -1,11 +1,34 @@
 // file factorial.cc
 
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <iostream>
+
+using namespace std;
 
 
+uint64_t factorial(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n * factorial(n-1);
+
+}
 
 
 int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+	   cout	<< "argc should be only 2" << endl;
+	   exit(EXIT_SUCCESS);
+	}
+
+	int i = atoi(argv[1]);
+	cout << "i:" << i << endl;
+	cout << "factorial:" << i << ":" << factorial(i) << endl;
 
 
 	return 0;
