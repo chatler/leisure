844b4d04928a89e46d571dd5de5e60b9633ede48
chatler <zhangquan@vip.163.com>
2016-09-18 17:19:40 +0800

    add some branch predictor testing in get_str.cc

--- cc/get_str.cc
+++ cc/get_str.cc
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <iostream>
 
 
 char* GetString1()
@@ -13,11 +17,43 @@ char* GetString2()
 	return p;
 }
 
+void testBranchPredictor()
+{
+	const int arraySize = 32768;
+	int sum = 0;
+	int* data = new int[arraySize];
+
+	srand(time(NULL));
+	for (int c = 0; c < arraySize; ++c)
+		data[c] = rand()%256;
+	data[0] = 129;
+	data[1] = 125;
+
+	/*
+	   for (int c = 0; c < arraySize; ++c)
+	   if (data[c] >= 128)
+	   sum += data[c];
+	   */
+
+	for (int c = 0; c < 2; ++c) {
+		int t = (data[c] - 128) >> 31;
+		std::cout << "t:[" << t << "], datac:[" << data[c] << "]\n";
+		sum += ~t & data[c];
+	}
+
+	if (data) {
+		delete [] data;
+		data = NULL;
+	}
+}
 
 int main(int argc, char* argv[])
 {
 	printf("GetString1 returns: %s. \n", GetString1());
 	printf("GetString2 returns: %s. \n", GetString2());
+	testBranchPredictor();
+
+
 
 	return 0;
 }
